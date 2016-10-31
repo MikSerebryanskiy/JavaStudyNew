bd2d2d6aa527ca1b15d98cd1384e0cbabdefe2e4
MikSerebryanskiy <smihv@mail.ru>
2016-10-31 19:50:23 +1000

    study

--- src/Excersise01/OperatorSwitch.java
+++ src/Excersise01/OperatorSwitch.java
@@ -1,10 +1,19 @@
 package Excersise01;
 
+import java.util.Scanner;
+
 /**
  * Created by user on 31.10.2016.
  */
 public class OperatorSwitch {
     public static void main(String[] args) {
-        String s =
+        System.out.println("Выберите из вариантов:");
+        System.out.println("1. Треугольник;");
+        System.out.println("2. Прямоугольник;");
+        System.out.println("3. Квадрат");
+        Scanner sc = new Scanner(System.in);
+        String 
+
+
     }
 }

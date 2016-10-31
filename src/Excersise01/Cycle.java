822727836f2965820b2467de5e1d26c9a5c6b929
MikSerebryanskiy <smihv@mail.ru>
2016-10-31 20:45:01 +1000

    third comment

---
+++ src/Excersise01/Cycle.java
@@ -0,0 +1,15 @@
+package Excersise01;
+
+/**
+ * Created by user on 31.10.2016.
+ */
+public class Cycle {
+    public static void main (String[] args) {
+        int x = 20;
+        do {
+            System.out.println(x);
+            x--;
+        }
+        while (x > 0);
+    }
+}
